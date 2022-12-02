e12e325125001f80d263a243538808558f95a170
kbiuser <s999760@pjwstk.edu.pl>
2022-12-02 18:57:25 +0100

    zadanie 4

--- PIP/src/Zadanie4 s999760.java
+++ PIP/src/Zadanie4 s999760.java
@@ -19,16 +19,57 @@ class zadanie1 {
 
 class zadanie4 {
     public static void main(String[] args) {
-        numbers();
-
-        }
-
-    public static void numbers() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Wprowadz pierwsza wartosc");
-        int wartosc1 = scan.nextInt();
-        System.out.println("Wprowadz druga wartosc");
-        int wartosc2 = scan.nextInt();
+        int liczba1, liczba2, wybor;
+        System.out.println("Wprowadz pierwsza liczbe");
+        liczba1 = scan.nextInt();
+        System.out.println("Wprowadz druga liczbe");
+        liczba2 = scan.nextInt();
+
+        System.out.println("Wybierz dzialanie");
+        System.out.println("1: suma");
+        System.out.println("2: roznica");
+        System.out.println("3: mnozenie");
+        System.out.println("4: dzielenie");
+        wybor = scan.nextInt();
+
+        switch (wybor){
+            case 1:
+                add(liczba1, liczba2);
+                System.out.println(add(liczba1, liczba2));
+            break;
+            case 2:
+                sub(liczba1, liczba2);
+                System.out.println(sub(liczba1, liczba2));
+            break;
+            case 3:
+                mul(liczba1, liczba2);
+                System.out.println(mul(liczba1, liczba2));
+            break;
+            case 4:
+                div(liczba1, liczba2);
+                System.out.println(div(liczba1, liczba2));
+            break;
+        }
     }
 
+    public static int add(int x, int y) {
+        int wynik = x + y;
+        return wynik;
+    }
+
+    public static int sub(int x, int y) {
+        int wynik = x - y;
+        return wynik;
+    }
+
+    public static int mul(int x, int y) {
+        int wynik = x * y;
+        return wynik;
+    }
+
+    public static int div(int x, int y) {
+        int wynik = x / y;
+        return wynik;
+    }
 }
