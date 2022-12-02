b70a5da49c29a0891684b698ff20fb63a87fde65
kbiuser <s999760@pjwstk.edu.pl>
2022-12-02 15:32:10 +0100

    zadanie 1

---
+++ PIP/src/Zadanie4 s999760.java
@@ -0,0 +1,16 @@
+class zadanie1 {
+    public static void main(String[] args) {
+        boolean CzyPada = true;
+        boolean CzySwieciSlonce = true;
+
+        if (CzyPada && !CzySwieciSlonce) {
+            System.out.println("plucha");
+        } else if (CzyPada && CzySwieciSlonce) {
+            System.out.println("tecza");
+        } else if (!CzyPada && CzySwieciSlonce) {
+            System.out.println("slonecznie");
+        } else if (!CzyPada && !CzySwieciSlonce){
+            System.out.println("pochmurno");
+        }
+    }
+}
